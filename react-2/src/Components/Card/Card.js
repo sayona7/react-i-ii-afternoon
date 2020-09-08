4055c655eed8d7142597cc82402226b22d6342c3
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-09-08 18:40:26 -0500

    card info

--- react-2/src/Components/Card/Card.js
+++ react-2/src/Components/Card/Card.js
@@ -2,8 +2,33 @@ import React, {Component} from "react";
 
 class Card extends Component {
     render() {
+        const {person, count, index} = this.props;
+
         return (
-            <div>Card</div>
+
+             <div className='card'>
+
+                <div className='users'>
+                    <h1>{person.name.first} {person.name.last}</h1>
+                    <h4>From: {person.city}, {person.country}</h4>
+                    <h4>Job Title: {person.title}</h4>
+                    <h4>Employer: {person.employer}</h4>
+
+                    <h4>Favorite Movies:
+                    <ul>
+                        <li>{person.favoriteMovies[0]}</li>
+                        <li>{person.favoriteMovies[1]}</li>
+                        <li>{person.favoriteMovies[2]}</li>
+                        </ul>
+                    </h4>
+                </div>
+
+                <div className='counter'>
+                    <h1>{index}/{count}</h1>
+                </div>
+
+            </div>
+
         )
     }
 
